d759d0b5b634ed05f2347c73d5b182b1e2a9502e
valois pascal <pascal.valois@free.fr>
2019-10-31 21:34:55 +0100

    light modification to be nice

--- ReverseEngineering/r2graphasm.py
+++ ReverseEngineering/r2graphasm.py
@@ -10,26 +10,26 @@ except:
   print ("Usage : %s <binary>" %(sys.argv[0]))
   sys.exit(2)
 
-try:
+try: 
   out=os.popen("radare2 -v").read()
-except:
+  if ("radare2 2." in out): version=2
+  if ("radare2 3." in out): version=3
+except: 
   print ("No radare2 installed ???")
   sys.exit(0)
-
-if ("radare2 2." in out): version=2
-if ("radare2 3." in out): version=3
 
 filename=os.path.basename(binary)
 
 r = r2pipe.open(binary)
 r.cmd("aaa")
 
-if (version==2):
+if (version==2): 
   r.cmd('ag main > main.dot')
 
-if (version==3):
+if (version==3): 
   r.cmd("s main")
   r.cmd('agfd > main.dot')
 
 os.system ("dot -Tjpg -o "+filename+".jpg main.dot")
 os.remove ("main.dot")
+
